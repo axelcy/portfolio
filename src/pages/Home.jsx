3d261881b7199dd8a95274ee8087838702c947f5
Axel Cymerman <axelcy@gmail.com>
2023-11-01 09:24:46 -0300

    :v:

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,16 @@ import './styles/Home.css'
 const Home = () => {
     return (
         <main>
-            <p className='code-block'>{'<h1>'}</p>
-            {/* <h1>git commit -m 'Landing Page Complete'</h1> */}
-            <h1>Hola, soy <span className='gradiant-text'>Axel</span> xd</h1>
-            <p className='code-block'>{'</h1>'}</p>
-            <p className='code-block'>{'</p>'}</p>
-            <p className='strong code-block'>Full Stack Developer /</p>
-            <p className='code-block'>{'</p>'}</p>
+            <header>
+                <p className='code-block'>{'<h1>'}</p>
+                    <h1 className='code-padding'>Hola, soy <span className='gradiant-text'>Axel</span> xd</h1>
+                <p className='code-block'>{'</h1>'}</p>
+            </header>
+            <div className='padding-left'>
+                <p className='code-block'>{'</p>'}</p>
+                <p className='strong code-block code-padding'>Full Stack Developer /</p>
+                <p className='code-block'>{'</p>'}</p>
+            </div>
         </main>
     )
 }
--- src/pages/Proyectos.jsx
+++ src/pages/Proyectos.jsx
@@ -4,13 +4,16 @@ import './styles/Proyectos.css'
 const Proyectos = () => {
     return (
         <main>
-            <p className='code-block'>{'<h1>'}</p>
-            {/* <h1>git commit -m 'Landing Page Complete'</h1> */}
-            <h1>Mis proyectos</h1>
-            <p className='code-block'>{'</h1>'}</p>
-            <p className='code-block'>{'</p>'}</p>
-                <Proyecto name='Chinchu-Ranks' image='/chinchu-ranks.png' />
-            <p className='code-block'>{'</p>'}</p>
+            <header>
+                <p className='code-block'>{'<h1>'}</p>
+                <h1 className='code-padding'>Mis proyectos</h1>
+                <p className='code-block'>{'</h1>'}</p>
+            </header>
+                <section className='lista-proyectos'>
+                    <Proyecto name='Chinchu-Ranks' image='/chinchu-ranks.png' />
+                    <Proyecto name='Chinchu-Ranks' image='/chinchu-ranks.png' />
+                    <Proyecto name='Chinchu-Ranks' image='/chinchu-ranks.png' />
+                </section>
         </main>
     )
 }
