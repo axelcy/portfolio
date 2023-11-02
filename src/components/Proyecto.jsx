a442a75386bcc3f2305319d805f5a5e44abfc3f5
Axel Cymerman <axelcy@gmail.com>
2023-11-02 11:42:27 -0300

    hover proyecto

--- src/components/Proyecto.jsx
+++ src/components/Proyecto.jsx
@@ -1,20 +1,25 @@
-import './styles/Proyecto.css'
-import { Link } from "react-router-dom"
+import "./styles/Proyecto.css";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Proyecto = ({ name, image, url }) => {
+    const [isHovered, setIsHovered] = useState(false)
 
-    const handleClick = () => window.open(url, '_blank')
-    
+    const handleClick = () => window.open(url, "_blank")
+
+    const handleMouseEnter = () => setIsHovered(true)
+    const handleMouseLeave = () => setIsHovered(false)
+
     return (
-        <article>
-            <p className='code-block'>{'<article>'}</p>
-            <div className='proyecto' onClick={handleClick}>
-                <h2>{name}</h2>
-                <img src={`/proyectos/${image}`} />
-            </div>
-            <p className='code-block'>{'</article>'}</p>
-        </article>
-    )
-}
+            <article>
+                <p className={`code-block ${isHovered ? 'hovered' : ''}`} >{"<article>"}</p>
+                    <div className="proyecto" onClick={handleClick} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                        <h2>{name}</h2>
+                        <img src={`/proyectos/${image}`} />
+                    </div>
+                <p className={`code-block ${isHovered ? 'hovered' : ''}`} >{"</article>"}</p>
+            </article>
+        )
+    }
 
-export default Proyecto
+export default Proyecto;
